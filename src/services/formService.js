816f905165b7cbf761cbb079f789fd45bd9bf19c
Muzammil Shakir <muzammil.shakir786@gmail.com>
2022-05-09 02:33:18 +0500

    Server Moved to Stickball Server

--- src/services/formService.js
+++ src/services/formService.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-const API_URL = "http://localhost:5000/api/form/";
+const API_URL = "https://stickball-server.herokuapp.com/form/";
 
 
 export default {
     getForms(userId){
         return axios
         // .get(API_URL + "getuserforms/" +userId)
-        .get(API_URL + "forms/")
+        .get(API_URL )
         .then(response =>{
             return response.data;
         })
@@ -15,7 +15,7 @@ export default {
     createForm(data){
         console.log(data);
         return axios
-        .post(API_URL + "create", data)
+        .post(API_URL , data)
         .then(response =>{
             console.log(response.data); 
             return response.data;
@@ -24,7 +24,7 @@ export default {
 
     getForm(formId){
         return axios
-        .get(API_URL + "form/"+formId)
+        .get(API_URL + formId)
         .then(response =>{
           //  console.log(response.data);
             return response.data;   
@@ -34,7 +34,7 @@ export default {
     autoSave(data){
         console.log(data);
         return axios
-        .put(API_URL + "/editform/", data)
+        .put(API_URL , data)
         .then(response =>{
               console.log(response.data);
               return response.data;   
